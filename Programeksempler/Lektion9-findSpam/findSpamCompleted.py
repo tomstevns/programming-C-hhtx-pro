ca5adfde42b0650e487ab732be34d8ecd2f3fa56
tomstevns <tomstevns@gmail.com>
2021-10-11 16:42:55 +0200

    Dette eksempel skal bruges til FLE-mødet

--- Programeksempler/Lektion9-findSpam/findSpamCompleted.py
+++ Programeksempler/Lektion9-findSpam/findSpamCompleted.py
@@ -1,37 +1,21 @@
-
 spamWords = ["køb","tilbud", "vundet", "tillykke"]   # Create new list
-print(spamWords)
+print("Søg efter følgedende angivne spamwords \n",spamWords)
 spamWords.append("spam")
-"""
-print(spamWords)
-
-print("len(spamWords) is:", len(spamWords))
-for p in range(len(spamWords)):
- print("Poppet element er: ", spamWords.pop(), "resterende elementer listen", spamWords)
-
-
-#print("spamWords efter pops",spamWords)
-"""
-
-
-
-testMail1 = "Dette er en testmail "
+testMail1 = "Dette er et tilbud fra Lars Larsen "
 testMail2 = "Dette er en testmaill hvor du har vundet 500 kr - tillykke"
 #lav teststreng med inputfil eller index af mange filer
-
 spamwordsfound = 0
-
 for i in range(len(spamWords)):
  if spamWords[i] in testMail1:
-  print("Spamword ", spamWords[i], "in spamlist is: ", spamWords[i] in testMail1)
+  print("Spamword ", spamWords[i], "in testMail1 is: ", spamWords[i] in testMail1)
   spamwordsfound += 1
-
 print("spam words found = ",spamwordsfound)
-#initier spawords og nulstil
 spamwordsfound = 0
 
+#initier spawords og nulstil
+spamwordsfound = 0
 for i in range(len(spamWords)):
  if spamWords[i] in testMail2:
-  print("Spamword ", spamWords[i], "in spamlist is: ", spamWords[i] in testMail2)
+  print("Spamword ", spamWords[i], "in testmail2 is: ", spamWords[i] in testMail2)
   spamwordsfound += 1
 print("spam words found = ",spamwordsfound)
