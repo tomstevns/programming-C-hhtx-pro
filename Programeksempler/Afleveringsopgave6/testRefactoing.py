04e67739ee25a6ce95a0c3fb7e101e4088281461
tomstevns <tomstevns@gmail.com>
2022-03-30 17:41:31 +0200

    with Hash lecture

---
+++ Programeksempler/Afleveringsopgave6/testRefactoing.py
@@ -0,0 +1,5 @@
+x = 3
+y = 8
+z= 17
+result = x+y+z
+print("result is",result)
---
+++ Programeksempler/Lektion16-Hasing/hashing.py
@@ -0,0 +1,7 @@
+text = 'Python Programming'
+
+# compute the hash value of text
+hash_value = hash(text)
+print(hash_value)
+
+# Output: -966697084172663693
